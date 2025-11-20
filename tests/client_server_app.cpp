33769489c6e0a6529c54a912d034ba25f1fbd602
Jaroslav Gargulak <gargulakj@gmail.com>
2025-11-20 17:20:54 +0100

    client-server connection works

--- tests/client_server_app.cpp
+++ tests/client_server_app.cpp
@@ -1,23 +1,82 @@
 #include <gtest/gtest.h>
 #include <thread>
 #include "client_server_app.h"
+#include "log.h"
 
-static ServerApp serverApp;
+std::unique_ptr<ServerApp> serverApp;
+std::unique_ptr<ClientApp> clientApp;
 
 void ServerApp::Init()
-{
-  m_tcpServer.Init();
-  //RegisterEvent()
+{  
+  Gap::log.Info("Server init");  
+  
+  int fd = m_tcpServer.Init();
+  RegisterEvent(fd, [this](int fd, Event evn){ onSocketEvent(fd, evn); });
+  m_tcpServer.Open(Gap::Endpoint{{"127.0.0.1"}, 10000});
+  
 }
+void ServerApp::onSocketEvent(int fd, Event evn)
+{
+  Gap::log.Info("ServerApp::onSocketEvent " + std::to_string(evn));  
+
+}
+
+void ClientApp::Init()
+{  
+  Gap::log.Info("Client init");  
+  
+  int fd = m_tcpClient.Init();
+  RegisterEvent(fd, [this](int fd, Event evn){ onSocketEvent(fd, evn); });
+  m_tcpClient.Connect(Gap::Endpoint{{"127.0.0.1"}, 10000});
+  
+}
+
+void ClientApp::onTerminate()
+{
+  m_tcpClient.Close();
+}
+
+void ClientApp::onSocketEvent(int fd, Event evn)
+{
+  Gap::log.Info("ClientApp::onSocketEvent " + std::to_string(evn));  
+
+}
+
 static void ServerLoop()
 {  
-  serverApp.Run();
+  serverApp = std::make_unique<ServerApp>();
+  
+  serverApp->Init();
+  serverApp->Run();
 }
 
 
-// Demonstrate some basic assertions.
-TEST(ServerTesting, BasicAssertions) {
+static void ClientLoop()
+{  
+  clientApp = std::make_unique<ClientApp>();
   
+  clientApp->Init();
+  clientApp->Run();
+}
+
+static void Terminate()
+{  
+  sleep(5);
+  clientApp->Terminate();
+  serverApp->Terminate();
+}
+
+
+
+// Test connection client to server.
+TEST(ClientServer, Connection) {  
   std::thread srvThread(ServerLoop);
+  sleep(1);
+  std::thread clientThread(ClientLoop);
+  sleep(1);
+  std::thread termThread(Terminate);
+  clientThread.join();
+  srvThread.join();
+  termThread.join();
   
 }
--- tests/client_server_app.h
+++ tests/client_server_app.h
@@ -1,10 +1,22 @@
 #include "process.h"
 #include "tcp/server.h"
+#include "tcp/client.h"
 
 class ServerApp : public Gap::Process
 {
   public:
-  private:
     void Init();
+  private:
+    void onSocketEvent(int fd, Event evn);
     Gap::TcpServer m_tcpServer;
+};
+
+class ClientApp : public Gap::Process
+{
+  public:
+    void Init();
+    virtual void onTerminate() override;
+  private:
+    void onSocketEvent(int fd, Event evn);
+    Gap::TcpClient m_tcpClient;
 };
