35672a66ae26758c099ca48f84b754cba631c42b
Jaroslav Gargulak <gargulakj@gmail.com>
2025-11-20 17:20:08 +0100

    termination supported, callbacks works

--- src/process/process.cpp
+++ src/process/process.cpp
@@ -1,18 +1,67 @@
-#include "process.h"
 #include <stdexcept>
 #include <system_error>
 #include <errno.h>
 #include <iostream>
+#include <sys/signalfd.h>
+#include <sys/epoll.h>
+#include "process.h"
+#include "log.h"
 
 namespace Gap {
 
 Process::Process()
 {
+  // Store thread ID
+  m_threadId = pthread_self();
+
+  // Fill structure with signals which are handle by FD.
+  sigset_t signals;
+  sigemptyset(&signals);
+  for(int sig : m_exitSignals)
+  {
+    sigaddset(&signals, sig);
+  }
+
+  // Block signals which is handle by FD
+  if (sigprocmask(SIG_BLOCK, &signals, NULL) == -1)
+  {
+    throw std::system_error({errno, std::generic_category()}, "Cannot block signals handled by FD!");
+  }
+
+  // Create FD for signal handling.
+  m_exitSignalFd = signalfd(-1, &signals, SFD_NONBLOCK);
+  if (m_exitSignalFd == -1)
+  {
+    throw std::system_error({errno, std::generic_category()}, "Cannot create FD for signal handling!");
+  }
+ 
+  // Epoll create
   m_epollFd = epoll_create1(0);
   if (m_epollFd == -1)
   {
-    throw std::runtime_error("Cannot create epoll instance!");
+    throw std::system_error({errno, std::generic_category()}, "Cannot create epoll instance!");
   }
+
+  // Register exit signal
+  RegisterEvent(m_exitSignalFd, [this](int fd, Event evn){ onTerminate(fd, evn); });
+}
+
+Process::~Process()
+{
+  if(m_epollFd != -1)
+  {
+    close(m_epollFd);
+  }
+  if(m_exitSignalFd != -1)
+  {
+    close(m_exitSignalFd);
+  }
+}
+
+void Process::onTerminate(int fd, Event event)
+{
+  m_run = false;
+  onTerminate();
 }
 
 void Process::RegisterEvent(int fd, Callback cb)
@@ -22,15 +71,18 @@ void Process::RegisterEvent(int fd, Callback cb)
     struct epoll_event ev;
     ev.events = EPOLLIN | EPOLLOUT | EPOLLET;
     ev.data.fd = fd;
-    
+
+    // Register epol event
     if (epoll_ctl(m_epollFd, EPOLL_CTL_ADD, fd, &ev) == -1)
     {
       throw std::system_error({errno, std::generic_category()}, "Failed to register file descriptor!");
-    }  
+    }
+    // Emplace next structure for getting event.
     m_epollEvents.emplace_back(epoll_event{});
+    // Store callback function
     m_epollCallback[fd] = cb;
 
-    std::cout << "Process::RegisterEvent - done FD: " << fd << "\n";
+    log.Info("Process::RegisterEvent for fd: " + std::to_string(fd));
   }
   else
   {
@@ -40,52 +92,73 @@ void Process::RegisterEvent(int fd, Callback cb)
 
 void Process::DeregisterEvent(int fd)
 {
+  // Deregister epol event.
   if (epoll_ctl(m_epollFd, EPOLL_CTL_DEL, fd, nullptr) == -1)
   {
     throw std::system_error({errno, std::generic_category()}, "Failed to deregister file descriptor!");
   }
-  m_epollEvents.pop_back();
+  // Remove callback funtion
+  m_epollCallback.erase(fd);
+  // Remove structure for getting event.
+  m_epollEvents.pop_back();  
 }
 
 void Process::Run()
 {
-  std::cout << "APP run" << "\n";
+  log.Info("Application started");
   int eventCount = 0;
-  while(m_epollEvents.size() > 0)
+
+  while(m_run)
   {
+    // Wait for event. Event are stored to vector m_epollEvents.
     int eventCount = epoll_wait(m_epollFd, &m_epollEvents[0], m_epollEvents.size(), -1);
     
+    log.Info("EPOLL events count: " + std::to_string(eventCount));
+
+    // Check all events.
     for(int eventIt = 0; eventIt < eventCount; eventIt++)
     {
       struct epoll_event& event = m_epollEvents[eventIt];
-      // Ready to read
-      if((event.events & EPOLLIN) == EPOLLIN)
-      {
-      }
-      // Ready to write
-      if((event.events & EPOLLOUT) == EPOLLOUT)
-      {
-      }
-      // Error
-      else
-      {
-      }
+
+      // Get stored data
       int fd = event.data.fd;
-      m_epollCallback[fd](fd);
+
+      log.Info("EPOLL event fd: " + std::to_string(fd));
+      // Callback is there?
+      if(m_epollCallback.contains(fd))
+      {
+        // Error
+        if((event.events & (EPOLLHUP | EPOLLERR)) > 0)
+        {
+          m_epollCallback[fd](fd, Event::Error);
+        }        
+        // Ready to read
+        if((event.events & EPOLLIN) == EPOLLIN)
+        {
+          m_epollCallback[fd](fd, Event::ReadyRead);
+        }
+        // Ready to write
+        if((event.events & EPOLLOUT) == EPOLLOUT)
+        {
+          m_epollCallback[fd](fd, Event::ReadyWrite);
+        }
+      }      
     }
     
+    // Epoll error
     if(eventCount == -1)
     {
       throw std::system_error({errno, std::generic_category()}, "epoll_wait error");
     }
-
-    std::cout << "epoll_wait end" << "\n";
-
-    for(int eventIt = 0; eventIt < eventCount; eventIt++)
-    {
-      std::cout << "FD Event " << m_epollEvents[eventIt].data.fd << "\n";
-    }
   }
-  std::cout << "APP finished" << "\n";
+  
+  log.Info("Application finished");
 }
+
+void Process::Terminate()
+{
+  log.Info("Application terminated");
+  pthread_kill(m_threadId, SIGINT);
+}
+
 }
--- src/process/process.h
+++ src/process/process.h
@@ -1,28 +1,43 @@
 #ifndef GAP_PROCESS_H
 #define GAP_PROCESS_H
 
-#include <sys/epoll.h>
 #include <map>
 #include <vector>
 #include <functional>
+#include <signal.h>
 
 namespace Gap {
 
 class Process
 {
   public:
-    using Callback = std::function<void(int)>;
+    enum Event {
+      ReadyRead,
+      ReadyWrite,
+      Error
+    };
+
+    using Callback = std::function<void(int, Event)>;
     
     Process();
+    virtual ~Process();
     void RegisterEvent(int fd, Callback cb);
     void DeregisterEvent(int fd);
     void Run();
-    
+    void Terminate();
+    virtual void onTerminate(){}
   
   private:
+    bool m_run {true};
     int m_epollFd {-1};
     std::vector<struct epoll_event> m_epollEvents;
     std::map<int,Callback> m_epollCallback;
+    std::vector<int> m_exitSignals {SIGINT, SIGQUIT};
+    int m_exitSignalFd {-1};
+    pthread_t m_threadId {0};
+
+    void onTerminate(int fd, Event event);
+
     
 };
 
